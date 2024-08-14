c4bdbc7dbca4b6e33eb6e52208d9974176c4e63d
Imposter <ara404.github@gmail.com>
2024-08-14 12:11:19 +0530

    Update Quick Sort.cpp

--- Quick Sort.cpp
+++ Quick Sort.cpp
@@ -1,81 +1,67 @@
 #include<iostream>
+using namespace std;
 
 class sort{
-    public:
-    int* arr;
-    int size;
-void values(int values[]) ;
-    void display();
+	public :
+		int* arr;
+		int size;
+		void values(int values[]) ;
+		void display() ;
 };
 
 void sort::values(int values[]){
 	arr=new int[size];
-for(int i=0;i<size;i++)
+	for(int i=0;i<size;i++)
 	{
 		arr[i]=values[i];
-	
 	}
 }
-
-
-void sort:: display(){
-  for(int i=0;i<size;i++)
-  {
-    cout<<arr[i]<<" ";
-  }
+void sort::display(){
+    for(int i=0;i<size;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
 }
 
 
 int partition(int arr[],int a,int b)
 {
 	int p=arr[b];
-int i=a-1;
-for(int j=a;j<b;j++)
-{
-if(arr[j]<=p)
+	int i=a-1;
+	for(int j=a;j<b;j++)
+	{
+		if(arr[j]<=p)
 		{
 			i++;
 			swap(arr[i],arr[j]);
-}
+		}
 	}
 	swap(arr[i+1],arr[b]);
 	return (i+1);
-
-		}
-	
-		
-	
-	
-
-
-
-
-Void Quick_sort(int arr[],int a,int b){
-if(a<b)
-{
-int p= partition(arr,a,b)
-Quick_sort(arr,a,p-1)
-Quick_sort(arr,p+1,b);
-
 }
 
 
+void Quick_sort(int arr[],int a,int b){
+	if(a<b)
+	{
+		int p=partition(arr,a,b);
+		Quick_sort(arr,a,p-1);
+		Quick_sort(arr,p+1,b);
+	}
+
 }
-
-
-
 
 
 int main(){
-int values =[23,42,65,91,36,72,02];
+int values[]={22,54,15,87,34,26,21,12,9,11};
 sort s;
 s.size=sizeof(values)/sizeof(values[0]);
 s.values(values);
-cout<<"the unsorted array is ";
-
+cout<<"Unsorted array\n";
 s.display();
-cout<<"the sorted array is ";
-quick_sort();
+cout<<"\nSorted array\n";
+Quick_sort(s.arr,0,s.size);
 s.display();
 return 0;
+
 }
