a6f9aafc3ce49323448726ba64d3471ad102164a
Imposter <ara404.github@gmail.com>
2024-08-16 14:10:40 +0530

    Update Selection Sort.cpp

--- Selection Sort.cpp
+++ Selection Sort.cpp
@@ -1,51 +1,64 @@
-#include<iostream.h>
-
+#include<iostream>
 using namespace std;
 
-class sort{ 
-    public
-      int *arr;
-      int size 
-      void display();
-      void value();
+class sort{
+	public :
+		int* arr;
+		int size;
+		void values(int values[]) ;
+		void display() ;
+		void Selection_sort();
 };
 
-
-void sort::display(){
-for(int i = 0;i<size; i++)
-{
-   cout<<arr[i];
+void sort::values(int values[]){
+	arr=new int[size];
+	for(int i=0;i<size;i++)
+	{
+		arr[i]=values[i];
+	}
+}
+void sort::	display(){
+    for(int i=0;i<size;i++)
+    {
+        cout<<arr[i]<<" "	;
+    }
 }
 
+
+
+
+void sort::Selection_sort(){
+	int min;	
+    for(int i=0;i<size-1;i++)
+    {
+		min=i;
+    	for(int j=i+1;j<size;j++)
+    	{
+    		if(arr[j] < arr[min])
+    		{
+				min=j;
+    		}
+    	}	
+    	swap(arr[i],arr[min]);
+    }
+    for(int i=0;i<size;i++)
+    {
+        cout<<arr[i]<<" ";	
+    }
 }
 
-
-void sort:: selection_sort{
-int min=0;
-for(int i = 0;i<size; i++)
-{
-   for(int j = i;j<size; j++)
-      {
-         if(arr[j]<arr[min])
-           {
-            min=j
-           }
-       }
- }
-
-}
 
 
 
 int main(){
-int values=[3,2,89,34,51,62,41,77,92,23];
-
+int values[]={22,54,15,87,34,26,21,12,9,11};
 sort s;
-cout<<"Unsorted array is ";
+s.size=sizeof(values)/sizeof(values[0]);
+s.values(values);
+cout<<"Unsorted array\n";
 s.display();
-cout<<"sorted array is ";
-Selection_sort();
-s.display();
-
+cout<<"\nSorted array\n";
+s.Selection_sort();
+return 0;
 
 }
