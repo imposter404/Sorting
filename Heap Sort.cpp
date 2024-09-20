7c7459f52f23db63175144a511af346ff370147e
Imposter <ara404.github@gmail.com>
2024-09-20 20:09:13 +0530

    Update Heap Sort.cpp

--- Heap Sort.cpp
+++ Heap Sort.cpp
@@ -29,7 +29,25 @@ void sort::display(){
 }
 
 
-
+void sort::Heapify(int n,int i)
+{
+	int largest =i;
+	int left=2*i +1;
+	int right=2*i +2;
+	if(left<n && arr[left]>arr[largest])
+	{
+		largest=left;
+	}
+	if(right<n && arr[right]>arr[largest])
+	{
+		largest=right;
+	}
+	if(largest != i )
+	{
+		swap(arr[i],arr[largest]);
+		Heapify(n,largest);
+	}
+}
 
 
 int main(){
