66f68d4cef86562383941ba97d83b023aa5a68ea
Imposter <ara404.github@gmail.com>
2024-09-21 21:34:54 +0530

    Update Counting Sort.cpp

--- Counting Sort.cpp
+++ Counting Sort.cpp
@@ -1,1 +1,13 @@
-#include<iostream> 
+#include<iostream>
+int main(){
+int values[]={4,2,2,8,3,3,1};
+sort s;
+s.size=sizeof(values)/sizeof(values[0]);
+s.values(values);
+cout<<"Unsorted array\n";
+s.display();
+cout<<"\nSorted array\n";
+s.Counting_sort();
+return 0;
+
+}
