cdc5a5b6e8008e27ae3698398dcfae5d90ef8365
Imposter <ara404.github@gmail.com>
2024-09-22 10:53:34 +0530

    Update Monkey Sort.cpp

--- Monkey Sort.cpp
+++ Monkey Sort.cpp
@@ -1,1 +1,22 @@
- #include<iostream>
+#include<iostream>
+#include<cstdlib>
+#include<time.h>
+using namespace std;
+
+
+
+
+
+int main(){
+int values[]={9,8,3,7,5,6,4,1};
+sort s;
+s.size=sizeof(values)/sizeof(values[0]);
+s.values(values);
+cout<<"Unsorted array\n";
+s.display();
+cout<<"\nSorted array\n";
+s.Monkey_sort();
+s.display();
+return 0;
+
+}
