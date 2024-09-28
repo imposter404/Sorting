74d06125bad778d47c51a1675cd3b4a69017618c
Imposter <ara404.github@gmail.com>
2024-09-28 09:08:44 +0530

    Update Shell Sort.cpp

--- Shell Sort.cpp
+++ Shell Sort.cpp
@@ -38,3 +38,17 @@ void sort::Shell_sort(){
 	}
 
 }
+
+int main(){
+int values[]={9,8,3,7,5,6,4,1};
+sort s;
+s.size=sizeof(values)/sizeof(values[0]);
+s.values(values);
+cout<<"Unsorted array\n";
+s.display();
+cout<<"\nSorted array\n";
+s.Shell_sort();
+s.display();
+return 0;
+
+}
